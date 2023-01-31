e4fb62425ed2eeec9fdd3f413b63230872229e89
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-01-31 22:28:11 +0900

    [Feat] 서버 쿼리 데이터 요청, 받아오기 성공

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getReviews } from "../api";
 import ReviewList from "./ReviewList";
 
@@ -15,10 +15,14 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoadClick = async () => {
-    const { reviews } = await getReviews();
+  const handleLoad = async (orderQuery) => {
+    const { reviews } = await getReviews(orderQuery);
     setItems(reviews);
   };
+
+  useEffect(() => {
+    handleLoad(order);
+  }, [order]);
 
   return (
     <div>
@@ -27,7 +31,6 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }

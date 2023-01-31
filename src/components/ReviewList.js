00c0fe67d3075d21053bbd255d9adad426128611
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-01-31 19:43:36 +0900

    [Add] 아이템 삭제 기능 구현

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -5,7 +5,9 @@ function formatDate(value) {
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 }
 
-function ReviewListItem({ item }) {
+function ReviewListItem({ item, onDelete }) {
+  const handleDeleteClick = () => onDelete(item.id);
+
   return (
     <div className="ReviewListItem">
       <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
@@ -14,18 +16,19 @@ function ReviewListItem({ item }) {
         <p>{item.rating}</p>
         <p>{formatDate(item.createdAt)}</p>
         <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>삭제</button>
       </div>
     </div>
   );
 }
 
-function ReviewList({ items }) {
+function ReviewList({ items, onDelete }) {
   return (
     <ul>
       {items.map((item) => {
         return (
           <li>
-            <ReviewListItem item={item} />
+            <ReviewListItem item={item} onDelete={onDelete} />
           </li>
         );
       })}

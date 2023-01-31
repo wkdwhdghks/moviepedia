851c677d2230a54ad20ec6de443b9f3bcad3e6da
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-02-01 01:12:29 +0900

    [Feat] 렌더링중에 버튼 비활성화 구현

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ function App() {
   const [order, setOther] = useState("createdAt");
   const [offset, setOffset] = useState(0);
   const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -21,11 +22,20 @@ function App() {
   };
 
   const handleLoad = async (options) => {
-    const { reviews, paging } = await getReviews(options);
+    let result;
+    try {
+      setIsLoading(true);
+      result = await getReviews(options);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+    const { reviews, paging } = result;
     if (options.offset === 0) {
       setItems(reviews);
     } else {
-      setItems([...items, ...reviews]);
+      setItems((prevItems) => [...prevItems, ...reviews]);
     }
     setOffset(options.offset + reviews.length);
     setHasNext(paging.hasNext);
@@ -46,7 +56,11 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      {hasNext && <button onClick={handleLoadMore}>더보기</button>}
+      {hasNext && (
+        <button disable={isLoading} onClick={handleLoadMore}>
+          더보기
+        </button>
+      )}
     </div>
   );
 }

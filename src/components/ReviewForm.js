2d6b983fb2b94e5b8150bced1bcab6446e97439c
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-02-02 17:50:48 +0900

    [Feat] 리뷰 생성 api연동

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
+import { createReview } from "../api";
 import FileInput from "./FileInput";
 import RatingInput from "./RatingInput";
 import "./ReviewForm.css";
 
+const INITIAL_VALUES = {
+  title: "",
+  rating: 0,
+  content: "",
+  imgFile: null,
+};
+
 export default function ReviewForm() {
-  const [values, setValues] = useState({
-    title: "",
-    rating: 0,
-    content: "",
-    imgFile: null,
-  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submittingError, setSubmittingError] = useState(null);
+  const [values, setValues] = useState(INITIAL_VALUES);
 
   const handleChange = (name, value) => {
     setValues((prevValues) => ({
@@ -23,9 +28,23 @@ export default function ReviewForm() {
     handleChange(name, value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(values);
+    const formData = new FormData();
+    formData.append("title", values.title);
+    formData.append("rating", values.rating);
+    formData.append("content", values.content);
+    formData.append("imgFile", values.imgFile);
+    try {
+      setSubmittingError(null);
+      setIsSubmitting(true);
+      await createReview(formData);
+    } catch (error) {
+      setSubmittingError(error);
+      return;
+    } finally {
+      setValues(INITIAL_VALUES);
+    }
   };
 
   return (
@@ -50,7 +69,10 @@ export default function ReviewForm() {
         value={values.content}
         onChange={handleInputChange}
       ></textarea>
-      <button type="submit">확인</button>
+      <button type="submit" disabled={isSubmitting}>
+        확인
+      </button>
+      {submittingError?.message && <div>{submittingError.message}</div>}
     </form>
   );
 }
